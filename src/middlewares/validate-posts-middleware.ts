0b1b297e54d2edb3d830cddcde8a5b9c7223956b
Anton Kliaschonak <PuY25@bk.ru>
2025-09-08 18:34:48 +0300

    Trim validate fix

--- src/middlewares/validate-posts-middleware.ts
+++ src/middlewares/validate-posts-middleware.ts
@@ -1,48 +1,52 @@
-import {NextFunction, Request, Response} from "express";
-import {body, validationResult} from "express-validator";
+import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 export const getPostsValidationErrorsMiddieWare = (
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-        next();
-    }
+  if (errors.isEmpty()) {
+    next();
+  }
 
-    const result = errors.array().map(({ msg }) => {
-        return msg;
-    });
+  const result = errors.array().map(({ msg }) => {
+    return msg;
+  });
 
-    res.status(400).send({ errorsMessages: result });
+  res.status(400).send({ errorsMessages: result });
 };
 
 export const titlePostMaxLengthValidate = body("title")
-    .isLength({min: 1, max: 30})
-    .withMessage({
-        message: "Title field is Required and  must be maximum 30 symbols",
-        field: "title",
-    });
+  .trim()
+  .isLength({ min: 1, max: 30 })
+  .withMessage({
+    message: "Title field is Required and  must be maximum 30 symbols",
+    field: "title",
+  });
 
 export const shortDescriptionPostMaxLengthValidate = body("shortDescription")
-    .isLength({min: 1, max: 100})
-    .withMessage({
-        message: "ShortDescription field is Required and  must be maximum 100 symbols",
-        field: "shortDescription",
-    });
+  .trim()
+  .isLength({ min: 1, max: 100 })
+  .withMessage({
+    message:
+      "ShortDescription field is Required and  must be maximum 100 symbols",
+    field: "shortDescription",
+  });
 
 export const contentPostMaxLengthValidate = body("content")
-    .isLength({min: 1, max: 1000})
-    .withMessage({
-        message: "Content field is Required and  must be maximum 1000 symbols",
-        field: "content",
-    });
+  .trim()
+  .isLength({ min: 1, max: 1000 })
+  .withMessage({
+    message: "Content field is Required and  must be maximum 1000 symbols",
+    field: "content",
+  });
 
 export const blogIdPostRequiredValidate = body("blogId")
-    .notEmpty()
-    .withMessage({
-        message: "BlogId field is Required",
-        field: "blogId",
-    });
+  .notEmpty()
+  .withMessage({
+    message: "BlogId field is Required",
+    field: "blogId",
+  });
