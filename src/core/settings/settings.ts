462d860fb0a200b365409ba4dc2d558c39ab60a9
Anton Kliaschonak <PuY25@bk.ru>
2025-09-25 06:16:43 +0300

    Get pagination for all posts

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,5 +1,5 @@
 export const SETTINGS = {
   PORT: process.env.PORT || 3000,
-  MONGO_URL: process.env.LOCAL_MONGO_URI_FOR_BLOGS_PROJECT || 'mongodb://localhost:27017',
+  MONGO_URL: process.env.MONGO_URI_FOR_BLOGS_PROJECT || 'mongodb://localhost:27017',
   DB_NAME: process.env.DB_NAME || 'db',
 };
